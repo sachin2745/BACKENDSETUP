5c290738cc8359fa191f3429168d179867b438a7
sachin2745 <sachin.official648@gmail.com>
2025-02-11 12:55:52 +0530

    In contact page faq is dynamic

--- backend/controllers/web/contactWebController.js
+++ backend/controllers/web/contactWebController.js
@@ -21,17 +21,19 @@ const getContactDetails = async (req, res) => {
         settingId  = 1 
     `;
 
-  
+  const faqSql = `
+  SELECT * FROM faq WHERE faqStatus = 0 ORDER BY faqSortBy ASC`
 
   try {
     // Execute the query for terms and condition
     const [contacts] = await db.execute(contactSql);
     const [settings] = await db.execute(settingSql);
+    const [faqs] = await db.execute(faqSql);
 
    
 
     // Return the data as JSON
-    return res.status(200).json({ contacts , settings });
+    return res.status(200).json({ contacts , settings ,faqs });
   } catch (err) {
     console.error("Error fetching contact page details:", err);
     return res.status(500).json({ error: err.message }); // Handle errors
--- frontend/src/app/(main)/contact-us/contactClient.jsx
+++ frontend/src/app/(main)/contact-us/contactClient.jsx
@@ -2,11 +2,12 @@
 import axios from "axios";
 import { useRouter } from "next/navigation";
 import React, { useEffect, useState } from "react";
-import { FaLocationArrow } from "react-icons/fa";
-
+import { FaLocationArrow, FaQuora } from "react-icons/fa";
+import { BsDot } from "react-icons/bs";
 const ContactUs = () => {
   const [contacts, setContacts] = useState({});
   const [settings, setSettings] = useState({});
+  const [faqs, setFaqs] = useState([]);
   const [loading, setLoading] = useState(true);
   const router = useRouter();
 
@@ -19,10 +20,12 @@ const ContactUs = () => {
         if (
           response.data.contacts &&
           response.data.settings &&
+          response.data.faqs &&
           response.data.contacts.length > 0
         ) {
           setContacts(response.data.contacts[0]);
           setSettings(response.data.settings[0]);
+          setFaqs(response.data.faqs);
         } else {
           router.push("/not-found");
         }
@@ -84,7 +87,7 @@ const ContactUs = () => {
   return (
     <div>
       <div className="container max-w-[78rem] mx-auto px-4 md:px-6 font-RedditSans">
-        <div className="mt-14">
+        <div className="my-14">
           <h1 className="my-4 text-4xl font-bold capitalize text-gray-800">
             {contacts.pageTitle}
           </h1>
@@ -99,7 +102,7 @@ const ContactUs = () => {
             )}
           </div>
 
-          <div className="flex flex-col md:flex-row justify-between my-12">
+          <div className="flex flex-col md:flex-row justify-between my-6 pt-6 pb-12  border-b-2 border-gray-200">
             <div className="w-full md:w-[48%]">
               <div className="mb-3">
                 <h2 className="text-4xl uppercase">{settings.companyName}</h2>
@@ -132,6 +135,36 @@ const ContactUs = () => {
               )}
             </div>
           </div>
+
+          {faqs.length > 0 && (
+            <div>
+              <h3 className="my-6  text-3xl font-bold capitalize text-quaternary">
+                Frequently Asked Questions
+              </h3>
+              <div className="mb-5">
+                {faqs.map((faq, index) => (
+                  <div
+                    key={index}
+                    className="collapse collapse-plus text-xs  sm:text-[16px] leading-relaxed bg-emerald-100 mb-6 border-b-4 border-emerald-300 rounded-lg focus:outline-none"
+                  >
+                    <input
+                      type="radio"
+                      name="faq-accordion"
+                      defaultChecked={index === 0} // First FAQ is open by default
+                    />
+                    <div className="collapse-title flex items-center gap-2 sm:gap-0 font-bold break-words">
+                      <FaQuora className="text-quaternary flex-shrink-0" />
+                      <span className="hidden sm:block mr-1">.</span>
+                      <span className="text-wrap">{faq.faqQuestion}</span>
+                    </div>
+                    <div className="collapse-content bg-white text-quaternary border-x-2 border-emerald-100">
+                      <p className="pt-4">{faq.faqAnswer}</p>
+                    </div>
+                  </div>
+                ))}
+              </div>
+            </div>
+          )}
         </div>
       </div>
     </div>
